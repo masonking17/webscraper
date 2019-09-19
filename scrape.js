ea6e92a8467071b779a2302796383a03783f8bb8
Mason J. King <mason.king@centre.edu>
2019-09-19 11:09:07 -0400

    list

--- scrape.js
+++ scrape.js
@@ -2,4 +2,18 @@ var scraper = require('./tools').scraper;
 //scraper = function(url,parseFcn,filename)
 //scrapes a URL, parses it with parseFcn, and archives the data in a JSON file called filename.
 
+var ParseFcn = function($,data)
+{
+    var parent= $("#az-list-A");
+    var children = parent.find("li")
+    children.toArray()
+    .forEach(function(item,index)
+             {
+             data.push($(item).text());
+            });
+};
+
+
+
+scraper("https://www.marvel.com/comics/characters",ParseFcn,"filename.json");
 
